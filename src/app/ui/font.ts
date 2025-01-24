66058ea170ba23440b2c8adb1dcbb8ed73ce87cd
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-01-24 19:15:16 +0800

    feat: font setup

---
+++ src/app/ui/font.ts
@@ -0,0 +1,16 @@
+import { Poppins } from "next/font/google";
+
+export const poppins_regular = Poppins({
+  weight: "300",
+  subsets: ["latin"],
+});
+
+export const poppins_medium = Poppins({
+  weight: "500",
+  subsets: ["latin"],
+});
+
+export const poppins_black = Poppins({
+  weight: "800",
+  subsets: ["latin"],
+});
