edebb34af1758c857f8d6e5e791a9656a0b794c2
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-01-25 04:17:39 +0800

    feat: artist story init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "./ui/hero-section";
+import { ArtistStory } from "./ui/artist-story";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <ArtistStory />
     </div>
   );
 }
