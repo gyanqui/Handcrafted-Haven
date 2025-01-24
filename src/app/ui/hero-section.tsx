a98fd870637a49a8aa75840c2f171b8f49e85ddb
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-01-24 19:44:44 +0800

    feat: white background

---
+++ src/app/ui/hero-section.tsx
@@ -0,0 +1,9 @@
+export function HeroSection() {
+    return (
+        <div className="bg-yellow-100 p-8 outline">
+            <div className="bg-white rounded-t-3xl p-6 outline">
+                This is Hero section
+            </div>
+        </div>
+    )
+}
