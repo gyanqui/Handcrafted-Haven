282d6f4b91437d0db244d46d9087f68b77f379e2
JosueSinaca <sinaca56@gmail.com>
2025-02-02 09:23:12 -0700

    updated

---
+++ src/app/ui/lib/definitions.ts
@@ -0,0 +1,49 @@
+// This file contains type definitions for your data.
+// It describes the shape of the data, and what data type each property should accept.
+export type Seller = {
+    seller_id: string;
+    seller_email: string;
+    address: string;
+    status: string;
+    introduction: string;
+  };
+
+export type Products = {
+    product_id: string;
+    name: string;
+    price: number;
+    quantity: number;
+    description: string;
+    image_url: string;
+    seller_id:string;
+    category_id: string;
+}
+
+export type User = {
+    user_id: string;
+    username: string;
+    email: string;
+    registration_date: Date;
+    password: string;
+    firstname: string;
+    lastname: string;
+    type: string;
+    profile_picture: string;
+  };
+  
+export type Category = {
+  category_id: string;
+  name: string;
+};
+export type Rating = {
+  rating_id: string;
+  title: string;
+  created_at: Date;
+  rating: string;
+  review: string;
+  product_id: string;
+  user_id: string;
+}
+
+
+  
