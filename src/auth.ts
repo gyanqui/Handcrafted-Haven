ccde9befe7f20405512c100c0da068f7a5593367
JosueSinaca <sinaca56@gmail.com>
2025-02-02 14:52:35 -0700

    making corrections

--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,41 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+import {z} from 'zod';
+import { sql } from '@vercel/postgres';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+async function getUser(email: string): Promise<User | undefined> {
+    try {
+      const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+      return user.rows[0];
+    } catch (error) {
+      console.error('Failed to fetch user:', error);
+      throw new Error('Failed to fetch user.');
+    }
+  }
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+        async authorize(credentials) {
+					const parsedCredentials = z
+						.object({ email: z.string().email(), password: z.string().min(6) })
+						.safeParse(credentials);
+
+					if (parsedCredentials.success) {
+						const { email, password } = parsedCredentials.data;
+						const user = await getUser(email);
+						if (!user) return null;
+						const passwordsMatch = await bcrypt.compare(password, user.password);
+
+						if (passwordsMatch) return user;
+					}
+					console.log('Invalid credentials');
+					return null;
+        },
+      }),
+	],
 });
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
