8d228ad83b65fea546c0cdb094a754debc91564c
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-02-03 19:06:47 +0800

    feat: footer

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import TopNav from "../ui/home/TopNav";
+import Footer from "../ui/home/Footer";
 export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full min-h-screen">
       <div>
         <TopNav />
       </div>
-      <div>{children}</div>
+      <div className="flex-grow bg-custom-grey">{children}</div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/app/ui/home/Footer.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { poppins } from "../font";
+import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
+
+export default function Footer() {
+    const thisYear = new Date().getFullYear();
+    return (
+        <div className="bg-black text-white flex flex-row items-center justify-between px-4 h-12">
+            <div>
+                <p className={`${poppins.className} font-extrabold`}>Handcrafted Haven</p>
+                <p className="text-custom-grey">&copy;{thisYear} WDD430 Group 11</p>
+            </div>
+            <div className="flex flex-row flex-wrap gap-1">
+                <Link href="https://facebook.com" target="_blank" className="bg-custom-yellow p-1 rounded-lg"><FaFacebookF className=" text-black text-xl"/></Link>
+                <Link href="https://instagram.com" target="_blank" className="bg-custom-yellow p-1 rounded-lg"><FaInstagram className=" text-black text-xl"/></Link>
+                <Link href="https://twitter.com" target="_blank" className="bg-custom-yellow p-1 rounded-lg"><FaTwitter className=" text-black text-xl"/></Link>
+                <Link href="https://linkedin.com" target="_blank" className="bg-custom-yellow p-1 rounded-lg"><FaLinkedinIn className=" text-black text-xl"/></Link>
+            </div>
+        </div>
+    )
+}
