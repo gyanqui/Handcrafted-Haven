149a24076a5246bb127660536786fb83277f4d80
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-02-04 16:10:06 +0800

    feat: route setting in side  menu

--- src/app/ui/home/SideMenu.tsx
+++ src/app/ui/home/SideMenu.tsx
@@ -4,6 +4,7 @@ import { BsCart } from "react-icons/bs";
 import { LuUsersRound } from "react-icons/lu";
 import { AiOutlineProduct } from "react-icons/ai";
 import { SlLogout } from "react-icons/sl";
+import Link from "next/link";
 
 interface SideMenuProps {
   toggleOpen: () => void;
@@ -13,25 +14,36 @@ export default function SideMenu({ toggleOpen }: SideMenuProps) {
     <div className="bg-black text-custom-yellow flex flex-col items-center h-full justify-between">
       {/* logo */}
       <div>
-        <h1 className="font-extrabold text-4xl">Handcrafted Haven</h1>
+        <Link href="/home" onClick={toggleOpen}>
+          <h1 className="font-extrabold text-4xl">Handcrafted Haven</h1>
+        </Link>
       </div>
 
       {/* items */}
       <div className="flex-grow py-8 flex flex-col justify-evenly text-2xl">
         <div>
-          <RxAvatar className="inline m-4" />
-          Dashboard
+          <Link href="/home/dashboard" onClick={toggleOpen}>
+            <RxAvatar className="inline m-4" />
+            Dashboard
+          </Link>
         </div>
         <div>
-            <BsCart className="inline m-4"/>Shopping Cart
+          <Link href='/home/shopping-cart' onClick={toggleOpen}>
+          <BsCart className="inline m-4" />
+          Shopping Cart
+          </Link>
         </div>
         <div>
+          <Link href='/home/artists' onClick={toggleOpen}>
           <LuUsersRound className="inline m-4" />
           Artists
+          </Link>
         </div>
         <div>
+          <Link href='/home/products' onClick={toggleOpen}>
           <AiOutlineProduct className="inline m-4" />
           Products
+          </Link>
         </div>
         <div>
           <SlLogout className="inline m-4" />
