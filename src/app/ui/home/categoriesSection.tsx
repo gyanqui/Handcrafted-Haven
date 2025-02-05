60d81b039cca4597f948688174fcc4b68b9b6a30
Gab Yanqui <gyanqui3@gmail.com>
2025-02-05 09:05:51 -0500

    Category nav responsice

--- src/app/ui/home/categoriesSection.tsx
+++ src/app/ui/home/categoriesSection.tsx
@@ -2,21 +2,29 @@ import Link from 'next/link';
 
 const CategoriesSection = () => {
   return (
-    <section className="bg-black py-10">
+    <section className="bg-custom-yellow md:bg-black py-10">
       <div className="container mx-auto px-5">
-        <div className="flex justify-between items-center">
-          <Link href="/jewelry-accessories">
-            <p className="text-custom-yellow font-bold text-lg hover:underline cursor-pointer">Jewelry & Accessories</p>
-          </Link>
-          <Link href="/home-decor">
-            <p className="text-custom-yellow font-bold text-lg hover:underline cursor-pointer">Home Decor</p>
-          </Link>
-          <Link href="/wearable-art">
-            <p className="text-custom-yellow font-bold text-lg hover:underline cursor-pointer">Wearable Art</p>
-          </Link>
-          <Link href="/stationery-paper-crafts">
-            <p className="text-custom-yellow font-bold text-lg hover:underline cursor-pointer">Stationery & Paper Crafts</p>
-          </Link>
+        <div className="flex flex-col md:flex-row justify-between items-center w-full text-center">
+          <div className="w-full border-b border-black">
+            <Link href="/jewelry-accessories">
+              <p className="text-black md:text-custom-yellow font-bold text-lg cursor-pointer py-3 hover:text-white">Jewelry & Accessories</p>
+            </Link>
+          </div>
+          <div className="w-full border-b border-black">
+            <Link href="/home-decor">
+              <p className="text-black md:text-custom-yellow font-bold text-lg cursor-pointer py-3 hover:text-white">Home Decor</p>
+            </Link>
+          </div>
+          <div className="w-full border-b border-black">
+            <Link href="/wearable-art">
+              <p className="text-black md:text-custom-yellow font-bold text-lg cursor-pointer py-3 hover:text-white">Wearable Art</p>
+            </Link>
+          </div>
+          <div className="w-full">
+            <Link href="/stationery-paper-crafts">
+              <p className="text-black md:text-custom-yellow font-bold text-lg cursor-pointer py-3 hover:text-white">Stationery & Paper Crafts</p>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
