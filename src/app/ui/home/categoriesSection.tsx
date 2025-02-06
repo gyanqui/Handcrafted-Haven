19dbfa3133ed83511c7da53f2b474f0c0e161adc
Gab Yanqui <gyanqui3@gmail.com>
2025-02-05 22:57:46 -0500

    Complete nav banner

--- src/app/ui/home/categoriesSection.tsx
+++ src/app/ui/home/categoriesSection.tsx
@@ -1,30 +1,31 @@
 import Link from 'next/link';
 
+const allCategories = [
+  { name: 'Jewelry & Accessories', href: '/jewelry-accessories' },
+  { name: 'Home Decor', href: '/home-decor' },
+  { name: 'Wearable Art', href: '/wearable-art' },
+  { name: 'Stationery & Paper Crafts', href: '/stationery-paper-crafts' },
+  { name: 'Category 5', href: '/category-5' },
+  { name: 'Category 6', href: '/category-6' },
+  { name: 'Category 7', href: '/category-7' },
+  { name: 'Category 8', href: '/category-8' },
+  { name: 'Category 9', href: '/category-9' },
+  { name: 'Category 10', href: '/category-10' },
+  { name: 'Category 11', href: '/category-11' }
+];
+
 const CategoriesSection = () => {
   return (
-    <section className="bg-custom-yellow md:bg-black py-10">
+    <section className="md:bg-black py-10">
       <div className="container mx-auto px-5">
-        <div className="flex flex-col md:flex-row justify-between items-center w-full text-center">
-          <div className="w-full border-b border-black">
-            <Link href="/jewelry-accessories">
-              <p className="text-black md:text-custom-yellow font-bold text-lg cursor-pointer py-3 hover:text-white">Jewelry & Accessories</p>
-            </Link>
-          </div>
-          <div className="w-full border-b border-black">
-            <Link href="/home-decor">
-              <p className="text-black md:text-custom-yellow font-bold text-lg cursor-pointer py-3 hover:text-white">Home Decor</p>
-            </Link>
-          </div>
-          <div className="w-full border-b border-black">
-            <Link href="/wearable-art">
-              <p className="text-black md:text-custom-yellow font-bold text-lg cursor-pointer py-3 hover:text-white">Wearable Art</p>
-            </Link>
-          </div>
-          <div className="w-full">
-            <Link href="/stationery-paper-crafts">
-              <p className="text-black md:text-custom-yellow font-bold text-lg cursor-pointer py-3 hover:text-white">Stationery & Paper Crafts</p>
-            </Link>
-          </div>
+        <div className="flex flex-row justify-start items-center w-full overflow-x-auto scrollbar-hide">
+          {allCategories.map((category, index) => (
+            <div key={index} className="min-w-max border-r border-black last:border-none">
+              <Link href={category.href}>
+                <p className="text-custom-yellow font-bold text-lg cursor-pointer py-3 px-5 hover:text-white">{category.name}</p>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </section>
