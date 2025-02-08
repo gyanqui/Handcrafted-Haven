55c121046a882a4a6b91feda5abcb9b6a865208f
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-02-08 23:46:38 +0800

    temp: working on searchArtisan

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import { sql } from "@vercel/postgres";
-import { ProductProps } from "./definitions";
 
 export async function listCategories() {
   try {
@@ -29,22 +28,41 @@ export async function searchProducts(query: string) {
             SELECT p.product_id, p.name, p.price, p.image_url AS image_url, 
                    ROUND(COALESCE(AVG(r.rating), 0), 1) AS averageRate
             FROM products p
-            LEFT JOIN reviews r ON p.product_id = r.product_id
-            LEFT JOIN sellers s ON p.seller_id = s.seller_id
-            LEFT JOIN users u ON s.user_id = u.user_id
             LEFT JOIN categories c ON p.category_id = c.category_id
+            LEFT JOIN reviews r ON r.product_id = p.product_id
 
             WHERE p.name ILIKE ${sqlQuery}
                 OR c.category ILIKE ${sqlQuery}
-                OR u.firstname ILIKE ${sqlQuery}
-                OR u.lastname ILIKE ${sqlQuery}
-                OR u.username ILIKE ${sqlQuery}
+                OR p.description ILIKE ${sqlQuery}
             GROUP BY p.product_id
         `;
 
     return data.rows;
   } catch (error) {
     console.error("Failed to search products:", error);
+    return [];
+  }
+}
+
+export async function searchArtisans(query: string) {
+  try {
+    const sqlQuery = `%${query}%`;
+
+    const data = await sql`
+      SELECT s.seller_id, s.seller_email, s.introduction, u.username, u.firstname, u.lastname, u.profile_image_url, s.address
+      FROM sellers s
+      JOIN users u on s.user_id = u.user_id
+      
+      WHERE s.seller_email ILIKE ${sqlQuery}
+      OR s.introduction ILIKE ${sqlQuery}
+      OR u.username ILIKE ${sqlQuery}
+      OR u.firstname ILIKE ${sqlQuery}
+      OR u.lastname ILIKE ${sqlQuery}
+      OR s.address ILIKE ${sqlQuery}
+    `;
+    return data.rows;
+  } catch (error) {
+    console.error("Failed to search artisans: ", error);
     return [];
   }
 }
--- src/app/query/route.ts
+++ src/app/query/route.ts
@@ -1,14 +1,47 @@
 import { sql } from "@vercel/postgres";
 async function listCategories() {
     const data = await sql`
-    SELECT * from categories`;
+        SELECT * from categories`;
 
     return data.rows;
 }
 
+async function listUsers() {
+    const data = await sql`
+        SELECT * from Users
+    `
+
+    return data.rows
+}
+
+async function listSellers() {
+    const data = await sql`
+        SELECT * FROM sellers
+    `
+    return data.rows
+}
+
+async function listProducts() {
+    const data = await sql`
+        SELECT * FROM products
+    `
+    return data.rows
+}
+
+async function listReviews() {
+    const data = await sql`
+        SELECT * FROM reviews
+    `
+    return data.rows
+}
+
 export async function GET() {
     try {
-        return Response.json(await listCategories());
+        const [categories, users, sellers, products, reviews] = await Promise.all([
+            listCategories(), listUsers(), listSellers(), listProducts(), listReviews()
+        ])
+        return Response.json({ categories, users, sellers, products, reviews })
+        ;
     } catch (error) {
         return Response.json({error}, {status: 500})
     }
