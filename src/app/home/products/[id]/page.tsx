36ea1ed63c726ddab15893701146b0c3db95d25e
CarlosYaelCervantesRojas <carlos.yael.cervantes.rojas@gmail.com>
2025-02-08 22:23:17 -0600

    details

--- src/app/home/products/[id]/page.tsx
+++ src/app/home/products/[id]/page.tsx
@@ -1,4 +1,6 @@
 import { fetchProductById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+import Image from 'next/image';
 
 export default async function Page(props: { params: Promise<{ id: string }>}) {
     const param = await props.params;
@@ -6,9 +8,39 @@ export default async function Page(props: { params: Promise<{ id: string }>}) {
 
     const product = await fetchProductById(id);
 
-    console.log(product);
+    // console.log(product); // comment later
+    if (!product) {
+        notFound(); 
+    }
+
+    const {
+        name,
+        price,
+        quantity,
+        description,
+        image_url
+    } = product;
 
     return (
-        <>Product ID {id} page</>
-    )
+        <section className='flex flex-col items-center my-5 gap-5'>
+            <Image className='w-full rounded-2xl shadow-xl md:w-1/2 lg:w-1/3'
+                src={image_url}
+                width={300}
+                height={400}
+                alt={name + "image."}
+            />
+            <aside className='mx-5 md:mx-16 lg:shadow-xl lg:rounded-2xl lg:mx-24 lg:p-5'>
+                <h3 className='text-2xl mb-3'>{name}</h3>
+                {quantity > 0 ?
+                    <span className='bg-slate-300 py-0.5 px-2 rounded-xl'>In Stock</span> :
+                    null
+                }
+                <p className='mt-3 text-4xl font-medium'>${price}.00</p>
+                <p className='my-3'>
+                    <span className='block underline decoration-slate-800 underline-offset-4'>Description</span>
+                    {description}
+                </p>
+            </aside>
+        </section>
+    );
 }
--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -3,15 +3,22 @@ import postgres from 'postgres';
 const sql = postgres({ ssl: "require" });
 
 export async function fetchProductById(id: string) {
-    try {
-        const data = await sql`
-          select
-            *
-          from products
-          where product_id = ${id}
-        `
-        return data[0];
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const data = await sql`
+      select
+        product_id,
+        name,
+        price,
+        quantity,
+        description,
+        image_url,
+        seller_id,
+        category_id
+      from products
+      where product_id = ${id};
+    `
+    return data[0];
+  } catch (error) {
+    console.log(error);
+  }
 }
