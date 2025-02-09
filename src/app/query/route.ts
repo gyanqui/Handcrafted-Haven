654edbc0c10b50fd92ed887071e9288da204b25d
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-02-09 11:31:26 +0800

    fix: database

--- src/app/query/route.ts
+++ src/app/query/route.ts
@@ -1,8 +1,9 @@
 import { sql } from "@vercel/postgres";
 async function listData() {
     const data = await sql`
-        SELECT *
-        FROM reviews
+        SELECT p.name, r.review
+        FROM products p
+        JOIN reviews r ON p.product_id = r.product_id
     `;
     return data.rows
 }
