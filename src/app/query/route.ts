0cc7c61dfc61b86351577363ab378adca5cba2c6
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-02-18 14:20:40 +0800

    fix: fetch products without reviews in what's new

--- src/app/query/route.ts
+++ src/app/query/route.ts
@@ -1,7 +1,18 @@
 import { sql } from "@vercel/postgres";
 async function listData() {
   const data = await sql`
-    SELECT * FROM sellers  
+    SELECT 
+	      p.product_id,
+	      p.name,
+	      p.image_url,
+	      p.price,
+	      ROUND(COALESCE(AVG(r.rating), 0), 1) AS "rating"
+      FROM products p
+      LEFT JOIN reviews r
+      ON r.product_id = p.product_id
+      GROUP BY p.product_id, p.created_at
+      ORDER BY p.created_at DESC
+      LIMIT 3;
   `
   return data.rows;
 }
